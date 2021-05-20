5ff7be5931e5ce6a366c605f07b24458275eb146
chegashi <mochegri@student.1337.ma>
2021-05-20 15:40:49 +0100

    add script

---
+++ init_ios.sh
@@ -0,0 +1,5 @@
+python3.8 -m venv env
+source env/bin/activate
+env/bin/pip3.8 install -r requirements.txt
+sed -i '' 's/from keras.backend.tensorflow_backend import set_session/from tensorflow.compat.v1.keras.backend import set_session/g' env/lib/python3.8/site-packages/textgenrnn/textgenrnn.pyenv/bin/python3.8 run.py &
+open http://127.0.0.1:5000/
