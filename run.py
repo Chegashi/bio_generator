33b4ddec0b0e0a6bb4a0855759391be6163e79d2
Mohamed chegri <41196762+Chegashi@users.noreply.github.com>
2021-05-20 17:55:30 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-#! /usr/bin/env python
-
+from os import systeme
 from bio_app import app
-
+systeme("sed -i '' 's/from keras.backend.tensorflow_backend import set_session/from tensorflow.compat.v1.keras.backend import set_session/g' env/lib/python3.8/site-packages/textgenrnn/textgenrnn.py")
 if __name__ == "__main__":
     app.run(debug=True)
     
